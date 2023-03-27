cc432652ada8fa7a7ce45e140a41b1e12e514085
Kal1219 <kaleb.cortes1208@gmail.com>
2023-03-27 11:05:14 -0600

    Initial Commit

---
+++ L3 SwiftUi/L3_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  L3_SwiftUiApp.swift
+//  L3 SwiftUi
+//
+//  Created by Kaleb on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct L3_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
